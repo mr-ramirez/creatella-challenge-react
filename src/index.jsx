53dd7f784f0646b0fc0be0616b63823c3036973e
Jacob Ramirez <mr.jacob.ramirez@gmail.com>
2019-08-10 15:19:38 -0500

    Webpack and Babel installed

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'typeface-roboto';
-import './index.css';
-import App from './App';
+// import './index.css';
+import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
